9ad662f2ef111eb45342b4290580f36873887e6d
Rettey <rettey8810-byte@users.noreply.github.com>
2026-07-27 18:43:26 +0500

    Add all missing certificate images to Achievements page (47 certificates total)

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -64,6 +64,288 @@ function Achievements() {
       issuer: 'Leading Hotels',
       year: '2021',
       image: '/Images/port_certi_(12).jpg'
+    },
+    {
+      name: 'Certificate 1',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(1).jpg'
+    },
+    {
+      name: 'Certificate 2',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(2).jpg'
+    },
+    {
+      name: 'Certificate 3',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(3).jpg'
+    },
+    {
+      name: 'Certificate 4',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(11).jpg'
+    },
+    {
+      name: 'Certificate 5',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(14).jpg'
+    },
+    {
+      name: 'Certificate 6',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(15).jpg'
+    },
+    {
+      name: 'Certificate 7',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(16).jpg'
+    },
+    {
+      name: 'Certificate 8',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(17).jpg'
+    },
+    {
+      name: 'Certificate 9',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(18).jpg'
+    },
+    {
+      name: 'Certificate 10',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(19).jpg'
+    },
+    {
+      name: 'Certificate 11',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(20).jpg'
+    },
+    {
+      name: 'Certificate 12',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(30).jpg'
+    },
+    {
+      name: 'Certificate 13',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(31).jpg'
+    },
+    {
+      name: 'Certificate 14',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(32).jpg'
+    },
+    {
+      name: 'Certificate 15',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(33).jpg'
+    },
+    {
+      name: 'Certificate 16',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(34).jpg'
+    },
+    {
+      name: 'Certificate 17',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(35).jpg'
+    },
+    {
+      name: 'Certificate 18',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(36).jpg'
+    },
+    {
+      name: 'Certificate 19',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(37).jpg'
+    },
+    {
+      name: 'Certificate 20',
+      issuer: 'Professional Certification',
+      year: '2018',
+      image: '/Images/port_certi_(38).jpg'
+    },
+    {
+      name: 'Language Certificate 1',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(1).jpg'
+    },
+    {
+      name: 'Language Certificate 2',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(2).jpg'
+    },
+    {
+      name: 'Language Certificate 3',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(3).jpg'
+    },
+    {
+      name: 'Language Certificate 4',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(4).jpg'
+    },
+    {
+      name: 'Language Certificate 5',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(5).jpg'
+    },
+    {
+      name: 'Language Certificate 6',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(6).jpg'
+    },
+    {
+      name: 'Language Certificate 7',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(7).jpg'
+    },
+    {
+      name: 'Language Certificate 8',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(8).jpg'
+    },
+    {
+      name: 'Language Certificate 9',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(9).jpg'
+    },
+    {
+      name: 'Language Certificate 10',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(10).jpg'
+    },
+    {
+      name: 'Language Certificate 11',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(11).jpg'
+    },
+    {
+      name: 'Language Certificate 12',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(12).jpg'
+    },
+    {
+      name: 'Language Certificate 13',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(13).jpg'
+    },
+    {
+      name: 'Language Certificate 14',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(14).jpg'
+    },
+    {
+      name: 'Language Certificate 15',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(15).jpg'
+    },
+    {
+      name: 'Language Certificate 16',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(16).jpg'
+    },
+    {
+      name: 'Language Certificate 17',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(17).jpg'
+    },
+    {
+      name: 'Language Certificate 18',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(18).jpg'
+    },
+    {
+      name: 'Language Certificate 19',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(19).jpg'
+    },
+    {
+      name: 'Language Certificate 20',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(20).jpg'
+    },
+    {
+      name: 'Language Certificate 21',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(21).jpg'
+    },
+    {
+      name: 'Language Certificate 22',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(22).jpg'
+    },
+    {
+      name: 'Language Certificate 23',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(23).jpg'
+    },
+    {
+      name: 'Language Certificate 24',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(24).jpg'
+    },
+    {
+      name: 'Language Certificate 25',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(25).jpg'
+    },
+    {
+      name: 'Language Certificate 26',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(26).jpg'
+    },
+    {
+      name: 'Language Certificate 27',
+      issuer: 'Language Institute',
+      year: '2017',
+      image: '/Images/lan_certi_(27).jpg'
     }
   ]
 
@@ -134,7 +416,7 @@ function Achievements() {
               <div className="stat-icon">
                 <i className="fas fa-certificate"></i>
               </div>
-              <div className="stat-number">12</div>
+              <div className="stat-number">50</div>
               <div className="stat-label">Certifications</div>
             </div>
           </div>
